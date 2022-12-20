70989af6bead116b61bf4ed82deff515d1fad868
varmaxon <v.varmax@yandex.ru>
2022-12-20 21:52:36 +0300

    added Turbo-Stream

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'form', to: 'calc#result'
+  post 'form', to: 'calc#result'
   get 'static_pages/landing_page'
   get 'dashboard', to: 'calc#input'
   # get 'static_pages/dashboard'
